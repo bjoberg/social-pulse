0d4a740f384140e8eeb1f4bd8da9f30c73d07817
Lucas Stefanski <stefaluc000@gmail.com>
2017-03-31 13:33:46 -0500

    Added material-ui to client

--- client/components/Contact/Contact.js
+++ client/components/Contact/Contact.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import Divider from 'material-ui/Divider';
+import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
-export function Home() {
+const style = {
+  marginLeft: 20,
+};
+
+export function Contact() {
   return (
     <div>
-      Contact Page
+      <h1>Contact Page</h1>
+      <Paper zDepth={2}>
+        <TextField hintText="First name" style={style} underlineShow={false} />
+        <Divider />
+        <TextField hintText="Middle name" style={style} underlineShow={false} />
+        <Divider />
+        <TextField hintText="Last name" style={style} underlineShow={false} />
+        <Divider />
+        <TextField hintText="Email address" style={style} underlineShow={false} />
+        <Divider />
+      </Paper>
+      <RaisedButton label="Submit" primary={true} style={{ margin: 20 }} />
     </div>
   );
 }
 
-export default Home;
+export default Contact;
