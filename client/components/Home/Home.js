4ff6a135565c6ce8df5602b01e55c586336d6ebf
Lucas Stefanski <stefaluc000@gmail.com>
2017-03-31 12:40:16 -0500

    Styling changes
    Added Roboto font. Global <h1> <h2> <p> and <body> styles.

--- client/components/Home/Home.js
+++ client/components/Home/Home.js
@@ -2,16 +2,14 @@ import React from 'react';
 import styles from './Home.css';
 export function Home() {
   return (
-    <div className={styles.home}>
-      <div className={styles.header}>
-        <h1> Pulse </h1>
-        <p className={styles.mainparagraph}>
-          <br />This is a paragraph. Bl ahBl ahBl ahBl ahBlahBl ahBlah B la hBlah Bla hBlahB lahBlah BlahB lahBla hBlah BlahBla hBla hBla hBlahBl ahB
-                lahBlahB lahBla hBla hBlahBlahB lah BlahBlahB lah BlahB lahBlahBl ahBlahBlahBla hBlahBl ahB lah BlahBl ahBlahBl ahBlahB lah BlahBlah
-          <br /> <br /> <br />
-        </p>
-        <h2> Testimonial 1 </h2>
-      </div>
+    <div>
+      <h1> Pulse </h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam ornare enim, in dignissim eros congue egestas. Nunc gravida metus vel eros laoreet, sed ultricies leo tristique. Nulla semper neque mauris, at aliquam tortor vestibulum quis. Donec pretium mauris erat, at iaculis mauris consectetur vel. Nulla neque turpis, varius vitae euismod non, volutpat pellentesque ex. Curabitur et massa arcu. Suspendisse volutpat hendrerit elementum. Aenean tellus libero, sollicitudin sit amet finibus vel, sollicitudin ut lectus. Fusce a libero purus. Etiam dapibus ut sem molestie tempor. In finibus dolor ac nisl tempor venenatis. Donec mollis mi sit amet sem ultricies hendrerit.
+
+          Etiam tincidunt ex et consequat efficitur. Sed quis est arcu. Aliquam dui velit, euismod non convallis eget, dapibus feugiat quam. Cras vitae pharetra augue. Sed eget erat sed nibh congue convallis. Curabitur hendrerit auctor est et venenatis. Ut fringilla eget ante nec accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque non sem sit amet lorem tempor commodo. Donec sit amet dapibus ante. Vivamus ullamcorper nulla at mi dignissim condimentum. Praesent faucibus odio ac elit semper sodales. Donec vel felis facilisis urna dictum ullamcorper.
+      </p>
+      <h2> Testimonial 1 </h2>
+      <h1></h1>
     </div>
   );
 }
