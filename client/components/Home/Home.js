9d42844c60257e2d79807fe9c72bed52dcc971b9
Lucas Stefanski <stefaluc000@gmail.com>
2017-03-31 13:02:36 -0500

    Added placeholder text

--- client/components/Home/Home.js
+++ client/components/Home/Home.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import styles from './Home.css';
+import { placeHolderText1, placeHolderText2 } from '../placeHolderText';
 export function Home() {
   return (
     <div>
-      <h1> Pulse </h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam ornare enim, in dignissim eros congue egestas. Nunc gravida metus vel eros laoreet, sed ultricies leo tristique. Nulla semper neque mauris, at aliquam tortor vestibulum quis. Donec pretium mauris erat, at iaculis mauris consectetur vel. Nulla neque turpis, varius vitae euismod non, volutpat pellentesque ex. Curabitur et massa arcu. Suspendisse volutpat hendrerit elementum. Aenean tellus libero, sollicitudin sit amet finibus vel, sollicitudin ut lectus. Fusce a libero purus. Etiam dapibus ut sem molestie tempor. In finibus dolor ac nisl tempor venenatis. Donec mollis mi sit amet sem ultricies hendrerit.
-
-          Etiam tincidunt ex et consequat efficitur. Sed quis est arcu. Aliquam dui velit, euismod non convallis eget, dapibus feugiat quam. Cras vitae pharetra augue. Sed eget erat sed nibh congue convallis. Curabitur hendrerit auctor est et venenatis. Ut fringilla eget ante nec accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque non sem sit amet lorem tempor commodo. Donec sit amet dapibus ante. Vivamus ullamcorper nulla at mi dignissim condimentum. Praesent faucibus odio ac elit semper sodales. Donec vel felis facilisis urna dictum ullamcorper.
-      </p>
-      <h2> Testimonial 1 </h2>
-      <h1></h1>
+      <h1>Pulse</h1>
+      <p>{placeHolderText1}</p>
+      <p>{placeHolderText2}</p>
     </div>
   );
 }
