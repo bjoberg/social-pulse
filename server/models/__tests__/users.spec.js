b867360543355c6166f286d358221b767c7a9d82
Brett Oberg <brett@obergmail.com>
2017-04-01 22:55:20 -0500

    Added test for 'getUserLastName' method
    In this commit I:
    - Deleted the dummyData file
    - updated the format of user.controller.js to work with eslint
    - updated user.js format to work with eslint
    - wrote the test for 'getUserLastName'
    - Deleted the post routes from mern.io skeleton
    - Updated user routes to work with eslint

--- server/models/__tests__/users.spec.js
+++ server/models/__tests__/users.spec.js
@@ -5,43 +5,94 @@ import User from '../user';
 
 import mongoose from 'mongoose';
 
-// Initial users added into test db
+// Test users
 const users = [
-  new User({ username: "test.user", first_name: "test", last_name: "user", password: "password123.1", email: "test.user.1@gmail.com"}),
-  new User({ username: "test.user2", first_name: "test2", last_name: "user2", password: "password123.2", email: "test.user.2@gmail.com"})
+  new User({ username: 'test.user', first_name: 'test', last_name: 'user', password: 'password123.1', email: 'test.user.1@gmail.com' }),
+  new User({ username: 'test.user2', first_name: 'test2', last_name: 'user2', password: 'password123.2', email: 'test.user.2@gmail.com' }),
 ];
 
 const mongooseInstance = new mongoose.Mongoose;
 
-test.beforeEach('connect to the database', async t => {  
-  User.remove(function (err) {
-    if (err) console.log("Model not removed.")
-  });
-  await mongooseInstance.createConnection("mongodb://localhost:27017/social-pulse-test");
-  await User.create(users, function(err){
-    if(err) console.error("Save failed.", err);
-    else console.log("Saved the user.");
+test.before('connect to the database', async () => {
+  // Connect to the mongo test database instance.
+  await mongooseInstance.createConnection('mongodb://localhost:27017/social-pulse-test');
+});
 
+test.beforeEach('refresh the data', async () => {
+  // Remove any unwanted, modified, users.
+  User.remove(err => {
+    if (err) {
+      console.log('Model not removed.');
+    } else {
+      console.log('Removed the users.');
+    }
+  });
+  // Add the new users to the test database.
+  await User.create(users, err => {
+    if (err) {
+      console.error('Save failed.', err);
+    } else {
+      console.log('Saved the user.');
+    }
   });
 });
 
-test.afterEach(t => {
-  User.remove(function (err) {
-    if (err) console.log("Model not removed.")
+test.afterEach(() => {
+  // Remove all of the modified users from the test database.
+  User.remove(err => {
+    if (err) {
+      console.log('Model not removed.');
+    } else {
+      console.log('Removed the users.');
+    }
   });
+});
 
+test.after(() => {
+  // Disconnect from the test database instance.
   mongooseInstance.disconnect(err => {
-    if(err) return console.log(err);
+    if (err) {
+      return console.log(err);
+    }
+    return console.log('Disconnected.');
   });
 });
 
-test.serial('Should correctly give number of Users', async t => {
+test.serial('test getUsers', async t => {
+  // 1. Setup
   t.plan(2);
 
+  // 2. Request
   const res = await request(app)
     .get('/api/v1/users')
     .set('Accept', 'application/json');
 
+  // 3. Test
   t.is(res.status, 200);
   t.deepEqual(users.length, res.body.users.length);
+});
+
+test.serial('test getUserLastName Method', async t => {
+  // 1. Setup
+  t.plan(3);
+  // Request all of the users in the db
+  let res = await request(app)
+    .get('/api/v1/users')
+    .set('Accept', 'application/json');
+  // Get the _id value of the first user in the database.
+  const userId = res.body.users[0]._id;
+
+  // 2. Request
+  res = await request(app)
+    .get(`/api/v1/user/${userId}/last_name`)
+    .set('Accept', 'application/json');
+
+  // 3. Test
+  const lastNameValue = res.body.user.last_name;
+  const lastNameExpected = users[0].last_name;
+  const userlengthValue = Object.keys(res.body.user).length;
+
+  t.is(res.status, 200);
+  t.deepEqual(userlengthValue, 2, [`REAMDE: value == ${userlengthValue} || expected == 2`]);
+  t.deepEqual(lastNameValue, lastNameExpected, [`REAMDE: value == ${lastNameValue} || expected == ${lastNameExpected}`]);
 });
--- server/models/user.js
+++ server/models/user.js
@@ -3,36 +3,38 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 // NOTE: ommitted sessionId
+// TODO: Add 'unique: true' to email and username
+// TODO: Add 'trim: true' to username, first_name, last_name, email
 const userSchema = new Schema({
-  username: {type: 'String', required: true},
-  first_name: {type: 'String', required: true},
-  last_name: {type: 'String', required: true},
-  password: {type: 'String', required: true},
-  email: {type: 'String', required: true},
-  email_is_verified: {type: 'Boolean', default: false, required: true},
-  signup_date: {type: 'Date', default: Date.now, required: true},
-  last_user_interaction: {type: 'Date', default: Date.now, required: true},
+  username: { type: 'String', required: true },
+  first_name: { type: 'String', required: true },
+  last_name: { type: 'String', required: true },
+  password: { type: 'String', required: true },
+  email: { type: 'String', required: true },
+  email_is_verified: { type: 'Boolean', default: false, required: true },
+  signup_date: { type: 'Date', default: Date.now, required: true },
+  last_user_interaction: { type: 'Date', default: Date.now, required: true },
   social_media: [
     {
-      social_title: {type: 'String'},
-      date_added: {type: 'Date', default: Date.now},
-      date_modified: {type: 'Date', default: Date.now},
-      auth_token: {type: 'String'}
+      social_title: { type: 'String' },
+      date_added: { type: 'Date', default: Date.now },
+      date_modified: { type: 'Date', default: Date.now },
+      auth_token: { type: 'String' }
     }
   ],
   notification_list: [
-    {type: mongoose.Schema.Types.ObjectId, ref: 'Notification'}
+    { type: mongoose.Schema.Types.ObjectId, ref: 'Notification' }
   ],
   notification_preferences: {
-    newsletter: {type: 'Boolean', default: true},
-    updates: {type: 'Boolean', default: true},
-    password_reminder: {type: 'Boolean', default: true},
-    backup_email_reminder: {type: 'Boolean', default: true}
+    newsletter: { type: 'Boolean', default: true },
+    updates: { type: 'Boolean', default: true },
+    password_reminder: { type: 'Boolean', default: true },
+    backup_email_reminder: { type: 'Boolean', default: true }
   },
   security: {
-    two_factor_auth: {type: 'Boolean', default: false},
-    backup_email: {type: 'String', default: null},
-    verification_code: {type: 'Number', default: null}
+    two_factor_auth: { type: 'Boolean', default: false },
+    backup_email: { type: 'String', default: null },
+    verification_code: { type: 'Number', default: null }
   }
 });
 
