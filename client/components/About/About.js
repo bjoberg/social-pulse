5cde880469e45a95dc3c367e5e30bb95c5e63e80
Kenny Gao <kgao9@wisc.edu>
2017-04-19 21:03:39 -0500

    Fixed typo in About.js

--- client/components/About/About.js
+++ client/components/About/About.js
@@ -6,8 +6,8 @@ export function About() {
       <h1>Social Pulse</h1>
       <p>
           We have created a platform for users to post pictures to multiple social media sites.
-          Suppose you have a picture that you want to share with your facebook friends, 
-	  twitter followers - well now you can at the click of a button.	  
+          Suppose you have a picture that you want to share with your facebook or Flickr friends, 
+	   - well now you can at the click of a button.	  
       </p>
       <p>If you want to get started, just hit Sign Up and you will be on your way!</p>		  
     </div>
